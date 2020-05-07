2196de34f6aed308648a2a60bc07b077c8152a90
spacehiker <filip.osin@gmail.com>
2020-05-07 21:26:30 +0200

    torus

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 I made this repository to test the three.js library.
 
-Source code based on the [tutorial](https://www.youtube.com/watch?v=AYa0aJ9xd_U) of *Hello Roman* . Thanks man!
+Source code based on [the tutorial](https://www.youtube.com/watch?v=AYa0aJ9xd_U) of *Hello Roman* . Thanks man!
--- index.js
+++ index.js
@@ -9,38 +9,55 @@ const colorYellow = new THREE.Color('hsl(40,100%,60%)');
 const colorLight = new THREE.Color('hsl(41,100%,95%)');
 const colorPink = new THREE.Color('hsl(306,100%,60%)');
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+/*const geometry = new THREE.BoxGeometry(6, 6, 6);
 const material = new THREE.MeshPhongMaterial({
     color: colorYellow,
     shininess: 80
 });
+
 console.log('hello');
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+*/
+const torusGeometry = new THREE.TorusGeometry(40, 6, 80, 400);
+const torusMaterial = new THREE.MeshBasicMaterial({ color: colorPink });
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+scene.add(torus);
 
+const knotGeometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+const knotMaterial = new THREE.MeshBasicMaterial({ color: colorYellow });
+const torusKnot = new THREE.Mesh(knotGeometry, knotMaterial);
+scene.add(torusKnot);
 
-const light = new THREE.PointLight(colorLight, 2);
+const light = new THREE.PointLight(colorLight, 10);
 
 light.position.z = 20;
 light.position.y = -20;
 light.position.x = -40;
 
-const light2 = new THREE.PointLight(colorLight, 1);
+const light2 = new THREE.PointLight(colorLight, 5);
 
 light2.position.z = 10;
 light2.position.y = 20;
 light2.position.x = 40;
 
 scene.add(light);
-scene.add(light2);
+//scene.add(light2);
 
-camera.position.z = 5;
+camera.position.z = 100;
 
 const animate = function() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    torus.rotation.x -= 0.03;
+    torus.rotation.y -= 0.03;
+
+
+    torusKnot.rotation.x += 0.02;
+    torusKnot.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 };
