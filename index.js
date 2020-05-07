27afc26d08eaf58daffd79152584ec31a6c5cf78
spacehiker <filip.osin@gmail.com>
2020-05-07 23:02:54 +0200

    changes

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+})
+
+
 
 const colorYellow = new THREE.Color('hsl(40,100%,60%)');
 const colorLight = new THREE.Color('hsl(41,100%,95%)');
 const colorGreen = new THREE.Color('hsl(81, 100%, 24%)');
 const colorShadow = new THREE.Color('hsl(100,100%,0%');
 
-//cubemap - not working
 
-const path = 'image/';
+//cubemap below - not working
+/*const path = 'image/';
 const format = '.jpg';
 const urls = [
     path + 'arid2_bk' + format, path + 'arid2_dn' + format,
-    path + 'arid2_ft' + format, path + 'arid2_lf' + format,
-    path + 'arid2_rt' + format, path + 'arid2_up' + format
+    path + 'arid2_up' + format, path + 'arid2_lf' + format,
+    path + 'arid2_rt' + format, path + 'arid2_ft' + format
 ];
 
 const reflectionCube = new THREE.CubeTextureLoader().load(urls);
@@ -28,7 +37,7 @@ refractionCube.mapping = THREE.CubeRefractionMapping;
 
 scene.background = reflectionCube;
 
-
+*/
 //creating cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshPhongMaterial({
@@ -40,7 +49,7 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
-
+//creating torus
 const torusGeometry = new THREE.TorusGeometry(40, 10, 80, 400);
 const torusMaterial = new THREE.MeshPhongMaterial({ color: colorGreen, shininess: 50 });
 const torus = new THREE.Mesh(torusGeometry, torusMaterial);
@@ -51,20 +60,17 @@ const knotMaterial = new THREE.MeshPhongMaterial({ color: colorYellow, shininess
 const torusKnot = new THREE.Mesh(knotGeometry, knotMaterial);
 scene.add(torusKnot);
 
-const light = new THREE.PointLight(colorLight, 0.1);
+const light = new THREE.PointLight(colorLight, 1);
 
 light.position.set(200, 400, 600);
 scene.add(light);
 
-const light2 = new THREE.PointLight(colorShadow, 5);
+const ambientLight = new THREE.AmbientLight(colorLight);
+scene.add(ambientLight);
 
-light2.position.z = 1;
-light2.position.y = 1;
-light2.position.x = 1;
 
-scene.add(light2);
 
-camera.position.z = 30;
+camera.position.z = 100;
 
 const animate = function() {
     requestAnimationFrame(animate);
